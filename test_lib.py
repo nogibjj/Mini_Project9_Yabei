f56913df2d0d6f7dce107bfb41f9229922f738d2
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-26 22:59:13 -0400

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -2,7 +2,7 @@ from lib import data_summary, load_data, data_visual
 import pandas as pd
 
 def test_summary():
-    my_df = pd.read_csv('cars.csv', sep=';')
+    my_df = pd.read_csv('https://raw.githubusercontent.com/yabeizeng1121/Mini_Project5/main/cars.csv', sep=';')
     summary = data_summary(my_df)
     mean = my_df['Weight'].mean()
     median = my_df['Weight'].median()
@@ -10,13 +10,13 @@ def test_summary():
     assert median == summary.loc['50%', 'Weight'], "Median test failed"
 
 def test_load_data():
-    data_path = 'cars.csv'
+    data_path = 'https://raw.githubusercontent.com/yabeizeng1121/Mini_Project5/main/cars.csv'
     df = load_data(data_path)
     assert isinstance(df, pd.DataFrame), "load_data should return a DataFrame"
     assert not df.empty, "DataFrame should not be empty"
 
 def test_data_visual():
-    data_path = 'cars.csv'
+    data_path = 'https://raw.githubusercontent.com/yabeizeng1121/Mini_Project5/main/cars.csv'
     df = load_data(data_path)
     try:
         data_visual(df)
