17a9fdbd0abd9feb9f3b570a11f514520aa89c0c
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-26 22:59:27 -0400

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from lib import load_data, data_summary, data_visual
 
 def main():
 
-    data_path = "cars.csv"
+    data_path = "https://raw.githubusercontent.com/yabeizeng1121/Mini_Project5/main/cars.csv"
     df = load_data(data_path)
     sum_data = data_summary(df)
 
